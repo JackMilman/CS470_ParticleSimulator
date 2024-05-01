e270058be190cb4f547e7c6b1c9e50a6628cb12b
JackMilman <jpm1228@gmail.com>
2024-05-01 17:29:45 -0400

    Updated spatial-hashing benchmark to have a smoother curve of input sizes

--- particle-simulator/spatial_hashing_benchmark.sh
+++ particle-simulator/spatial_hashing_benchmark.sh
@@ -3,7 +3,7 @@
 for size in 0.005 0.0025
 do
     echo "Particle Size: ${size}"
-    for n_particles in 1000 2500 5000 10000 20000
+    for n_particles in 100 250 500 1000 2500 5000 7500 10000 15000 20000
     do 
         echo "---${n_particles} Particles---"
         timeout 100 ./app -n $n_particles -s $size
